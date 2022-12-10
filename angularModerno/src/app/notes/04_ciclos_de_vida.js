17c141223c12ab7124ea9d5bf567dad6b1943332
Daniel <studiosdopplermusic@gmail.com>
2022-12-09 23:23:56 -0500

    ciclo de vida de los componentes

---
+++ angularModerno/src/app/notes/04_ciclos_de_vida.js
@@ -0,0 +1,27 @@
+/**
+ * ONCHANGES
+ * - Se ejecuta de primero SOLO cuando hay un input o output
+ *
+ * OnInit
+ * - Cuando se inicializa el componente
+ *
+ * DoCheck
+ * - Para validación manual en un componente con custom change detection
+ *
+ * AfterContenInit
+ * - Despues de que el componente es inicializado
+ *
+ * AfterContentChecked
+ * - Despues que se hace la valicación del componente
+ *
+ * AfterViewInit
+ * - Cuando se termina de renderizar el contenido en elHTML
+ *
+ * AfterViewChecked
+ *-  Despues de la validación de contenido del componente
+ *
+ * OnDestroy
+ * - Al finalzar el ciclo de vida del componente
+ *
+ *
+ * */
