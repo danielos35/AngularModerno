4e380db559205882af4cd48f6d9bf20f22496ebe
Daniel <studiosdopplermusic@gmail.com>
2023-01-16 21:30:14 -0500

    View hero

--- angularModerno/src/app/heroes/heroes-m-d/pages/heroe/heroe.component.ts
+++ angularModerno/src/app/heroes/heroes-m-d/pages/heroe/heroe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { getHeroByID } from '../../helpers/getHeroById';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-heroe',
@@ -10,15 +11,22 @@ import { getHeroByID } from '../../helpers/getHeroById';
 export class HeroeComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly rutas: Router
+    private readonly rutas: Router,
+    private readonly location: Location
   ) {}
   dataHeroe: any;
+  url!: string;
 
   ngOnInit(): void {
     this.route.params.subscribe((res: Params) => {
       this.dataHeroe = getHeroByID(res['IdHeroe']);
+      this.url = `assets/heroes/${this.dataHeroe.id || ''}.jpg`;
       if (!this.dataHeroe)
         this.rutas.navigate(['/heroes/heores_intranet/main/marvel']);
     });
+  }
+
+  regresar() {
+    this.location.back();
   }
 }
