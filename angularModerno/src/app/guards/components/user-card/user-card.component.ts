0ccbcc548951b554f412a1c7df2d30c27490eb20
Daniel <studiosdopplermusic@gmail.com>
2023-01-17 22:32:52 -0500

    refactor search

--- angularModerno/src/app/guards/components/user-card/user-card.component.ts
+++ angularModerno/src/app/guards/components/user-card/user-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
+  name!:string;
+  constructor(private readonly route: ActivatedRoute,){
+  }
+
+  ngOnInit(): void {
+      this.route.queryParams.subscribe((res:any)=>{
+        this.name = res.name
+      })
+  }
 
 }
