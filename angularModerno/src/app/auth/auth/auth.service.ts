b9ce617f8ca94abc587783767db9c192966538b3
Daniel <studiosdopplermusic@gmail.com>
2023-01-19 22:07:11 -0500

    Manejo de errores

--- angularModerno/src/app/auth/auth/auth.service.ts
+++ angularModerno/src/app/auth/auth/auth.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-
+import { catchError, of, throwError } from 'rxjs';
 @Injectable({providedIn:'root'})
 export class AuthService {
+
+    // MANEJANDO LOS ERRORES DIRECTAMENTE EN EL SERVICIO
     constructor(private http:HttpClient){
         
     }
     key:string = 'AIzaSyCYX3e8xHsK477d4k3c-CaJp0G0qA1iXtU'
     signup(email:string, password:string, returnSecureToken:boolean){
+
         return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.key}`,{
             email, password,returnSecureToken
-        })
+        }).pipe(
+            catchError( (errorRes:any)=> {
+                return of('Hemos detectado un error')
+            })
+        )
     }
 }
