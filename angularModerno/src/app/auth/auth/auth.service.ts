f6e4e32dab93ebfa7523893e91b6de8c17446e54
Daniel <studiosdopplermusic@gmail.com>
2023-01-19 22:23:41 -0500

    Manejo de errores centralizado

--- angularModerno/src/app/auth/auth/auth.service.ts
+++ angularModerno/src/app/auth/auth/auth.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, of, throwError } from 'rxjs';
 @Injectable({providedIn:'root'})
 export class AuthService {
 
     // MANEJANDO LOS ERRORES DIRECTAMENTE EN EL SERVICIO
-    constructor(private http:HttpClient){
-        
-    }
-    key:string = 'AIzaSyCYX3e8xHsK477d4k3c-CaJp0G0qA1iXtU'
-    signup(email:string, password:string, returnSecureToken:boolean){
+    constructor(private http:HttpClient){}
+    key:string = 'AIzaSyCYX3e8xHsK477d4k3c-CaJp0G0qA1iXtU';
 
+    signup(email:string, password:string, returnSecureToken:boolean){
         return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.key}`,{
             email, password,returnSecureToken
         }).pipe(
-            catchError( (errorRes:any)=> {
-                return of('Hemos detectado un error')
-            })
+            catchError( this.manejoDeErrores )
         )
+    }
+
+    login(email:string, password:string,returnSecureToken:boolean = true){
+        return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.key}`,{
+            email, password,returnSecureToken
+        }).pipe(
+            catchError( this.manejoDeErrores )
+        )
+    }
+
+    manejoDeErrores(errores:HttpErrorResponse){
+        return of('Error');
     }
 }
