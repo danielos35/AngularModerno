fb00bf8b41c5c7164617d344c9747da07e6bd3b9
studiosdopplermusic@gmail.com <studiosdopplermusic@gmail.com>
2023-01-22 21:34:46 -0500

    Refactor delete

--- angularModerno/src/app/ngrx/components/pedidos/store/pedidos.reducer.ts
+++ angularModerno/src/app/ngrx/components/pedidos/store/pedidos.reducer.ts
@@ -5,22 +5,26 @@ const initalState:any = {
   pedidos: ['Zapatos']
 }
 
-export function pedidosReducer( state:any = initalState , action: PedidosActions.RemovePedidosClass | PedidosActions.AddPedidosClass):any | Action{
+export
+  function pedidosReducer
+    (
+      state:any = initalState ,
+      action: PedidosActions.RemovePedidosClass | PedidosActions.AddPedidosClass
+    ):any | Action
+    {
 
-  console.log(state);
-
-  switch(action.type){
-    case PedidosActions.ADD_PEDIDOS:
-      return {
-        ...structuredClone(state),
-        pedidos:[ ...state.pedidos, action.pedido]
-      }
-    case PedidosActions.REMOVE_PEDIDO:
-      return {
-        ...structuredClone(state),
-        pedidos: state.pedidos.filter( (_:any, i:number) => (i !==  action?.index ))
-      }
-    default:
-      return state
+    switch(action.type){
+      case PedidosActions.ADD_PEDIDOS:
+        return {
+          ...structuredClone(state),
+          pedidos:[ ...state.pedidos, action.pedido]
+        }
+      case PedidosActions.REMOVE_PEDIDO:
+        return {
+          ...structuredClone(state),
+          pedidos: state.pedidos.filter( (_:any, i:number) => (i !==  action?.index ))
+        }
+      default:
+        return state
   }
 }
